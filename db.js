8f430e4f8e94f311649c66850a61e9a3ff595739
Jarrett Helton <jaydhelton@gmail.com>
2020-12-31 15:24:18 -0500

    fixing bugs

--- db.js
+++ db.js
@@ -8,10 +8,18 @@ module.exports = function buildDB(db) {
     if (existing) {
       return tacoCollection.findOneAndUpdate(
         { uid: doc.uid },
-        { $inc: { tacos: doc.tacos } }
+        { $inc: { tacos: doc.tacos } },
+        { returnOriginal: false }
       );
     }
-    return tacoCollection.insertOne(doc);
+    return tacoCollection.findOneAndUpdate(
+      { uid: doc.uid },
+      { $set: doc },
+      {
+        upsert: true,
+        returnOriginal: false,
+      }
+    );
   }
 
   async function getActiveClubSession() {
